1b29dabb4b159177af169df989e0002d57e6c247
tom-aglow <tom@ohhhh.me>
2017-12-10 17:18:49 -0800

    create webpack.config for the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+
+const Home = require('./client/components/Home.jsx').default;
+
 
 const app = express();
 
 
+
 app.get('/', (req, res) => {
+  const content = renderToString(<Home/>);
 
-})
+  res.send(content);
+});
 
+
+//  SERVER SETUP
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
-})
+});
---
+++ server/webpack.server.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  //  Inform webpack that we're building a bundle from NodeJS, rather then for the browser
+  target: 'node',
+
+  //  Tell webpack the root file of our server application
+  entry: './src/index.js',
+
+  //  Tell webpack where to put the output file that is generated
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  //  Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            [
+              'env',
+              {
+                targets: {
+                  browser: ['last 2 versions']
+                }
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  }
+};
