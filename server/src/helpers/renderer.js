02cbedff8a97e583d79594aee9f106e356316b90
tom-aglow <tom@ohhhh.me>
2017-12-10 19:32:15 -0800

    routing setup

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import Routes from '../client/Route';
 
-import Home from '../client/components/Home';
-
-
-export default () => {
-  const content = renderToString(<Home />);
+export default ({path}) => {
+  const content = renderToString(
+    <StaticRouter location={path} context={{}}>
+      <Routes />
+    </StaticRouter>
+  );
 
   return `
     <html>
