683084de40e1fb257adf24b04a9cdbf676bd7033
tom-aglow <tom@ohhhh.me>
2017-12-10 20:34:57 -0800

    create Redux store on client and server

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import Routes from './Route';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
-  , document.querySelector('#root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root')
+);
--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+
 import Routes from '../client/Route';
 
-export default ({path}) => {
+export default ({path}, store) => {
   const content = renderToString(
-    <StaticRouter location={path} context={{}}>
-      <Routes />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
   );
 
   return `
